5fcb8b8c7b5a446c2bf90d3ebbddf7ac1f04fd2b
francongutierrez <francogutierrez04@gmail.com>
2024-12-26 12:20:41 -0300

    Commit inicial

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>¡Hola desde Vue!</h1>
+      <p>Este es un componente de Vue.js integrado con Laravel.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "App",
+  };
+  </script>
+  
